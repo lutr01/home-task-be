4d2a5e7c7564419e6aa6087182ece2466a92143f
Lu <lubova.tretjakova@gmail.com>
2020-02-26 13:13:37 +0100

    fixed the regex to be more strict

--- cypress/fixtures/user/user-data/validation.js
+++ cypress/fixtures/user/user-data/validation.js
@@ -1,6 +1,6 @@
 export const validateUserEmail = email => {
-    const emailRegex = /\S+@\S+\.\S+/
+    const emailRegex = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/
 
-    cy.task('log', `validating the email address ${email} to be of a format any_string@any_string.any_string`)
+    cy.task('log', `validating the email address ${email} to be of a correct format`)
     expect(email).to.match(emailRegex)
 }
